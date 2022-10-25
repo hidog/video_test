e38a103eacaed93c9488ff8a1eea68c9348aa0ed
hidog <hidog.gao@gmail.com>
2022-10-25 14:13:40 +0800

    fix bug

--- media.c
+++ media.c
@@ -369,33 +369,36 @@ int   flush_audio( Decode dec, Encode *enc, FifoBuffer fifobuf )
 
    // flush fifo buffer
    int      sample      =  av_audio_fifo_size(fifobuf.fifo);
-   AVFrame  *frame      =  av_frame_alloc();
-   frame->nb_samples    =  sample;
-   frame->format        =  enc->audio_ctx->sample_fmt;
-   frame->sample_rate   =  enc->audio_ctx->sample_rate;
-   av_channel_layout_copy( &frame->ch_layout, &enc->audio_ctx->ch_layout );
-   av_frame_get_buffer( frame, 0 );
-   if( av_frame_make_writable(frame) < 0 )
-      return ERROR;
-
-   ret   =  av_audio_fifo_read( fifobuf.fifo, (void **)frame->data, sample );
-   if( ret < sample )
-      return ERROR;
-
-   frame->pts   =  av_rescale_q( enc->sample_count, (AVRational){1, enc->audio_ctx->sample_rate}, enc->audio_stream->time_base );
-   enc->sample_count += sample; 
-
-   ret   =  avcodec_send_frame( enc->audio_ctx, frame );
-   if( ret < 0 )
-      return ERROR;  
-   ret   =  avcodec_receive_packet( enc->audio_ctx, enc->pkt );
-   if( ret >= 0 )
+   if( sample > 0 )
    {
-      av_packet_rescale_ts( enc->pkt, enc->audio_ctx->time_base, enc->audio_stream->time_base );
-      enc->pkt->stream_index   =  enc->audio_stream->index;
-      av_interleaved_write_frame( enc->fmt_ctx, enc->pkt );
+      AVFrame  *frame      =  av_frame_alloc();
+      frame->nb_samples    =  sample;
+      frame->format        =  enc->audio_ctx->sample_fmt;
+      frame->sample_rate   =  enc->audio_ctx->sample_rate;
+      av_channel_layout_copy( &frame->ch_layout, &enc->audio_ctx->ch_layout );
+      av_frame_get_buffer( frame, 0 );
+      if( av_frame_make_writable(frame) < 0 )
+         return ERROR;
+
+      ret   =  av_audio_fifo_read( fifobuf.fifo, (void **)frame->data, sample );
+      if( ret < sample )
+         return ERROR;
+
+      frame->pts   =  av_rescale_q( enc->sample_count, (AVRational){1, enc->audio_ctx->sample_rate}, enc->audio_stream->time_base );
+      enc->sample_count += sample; 
+
+      ret   =  avcodec_send_frame( enc->audio_ctx, frame );
+      if( ret < 0 )
+         return ERROR;  
+      ret   =  avcodec_receive_packet( enc->audio_ctx, enc->pkt );
+      if( ret >= 0 )
+      {
+         av_packet_rescale_ts( enc->pkt, enc->audio_ctx->time_base, enc->audio_stream->time_base );
+         enc->pkt->stream_index   =  enc->audio_stream->index;
+         av_interleaved_write_frame( enc->fmt_ctx, enc->pkt );
+      }
+      av_frame_free(&frame);
    }
-   av_frame_free(&frame);
 
    // flush audio encoder.
    ret   =  avcodec_send_frame( enc->audio_ctx, NULL );
@@ -531,6 +534,7 @@ int   open_g711_input( char *filename, Decode *dec, enum AVCodecID codeid, int c
    
    if( avformat_find_stream_info( fmt_ctx, &codec_opts ) < 0 ) 
       return ERROR;
+   av_dict_free(&codec_opts);
    
    // audio
    audio_index =  av_find_best_stream( fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0 );
